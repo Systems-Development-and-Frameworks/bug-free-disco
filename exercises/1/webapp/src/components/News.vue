488facca1e18cfdd0ac44865ec745e773fbb5302
fokokouti <fokoboris@gmail.com>
2020-10-17 14:38:39 +0200

    Updated News.vue clean unused style scoped

--- exercises/1/webapp/src/components/News.vue
+++ exercises/1/webapp/src/components/News.vue
@@ -41,15 +41,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
