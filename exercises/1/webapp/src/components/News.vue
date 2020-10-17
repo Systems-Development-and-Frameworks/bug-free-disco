5f410b7ad08f65eef79bf2e1f29fb58bd12ca4b2
fokokouti <fokoboris@gmail.com>
2020-10-17 14:35:55 +0200

    Added webapp - Hacker News solution for exercise-1

---
+++ exercises/1/webapp/src/components/News.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <h1 class="text-danger">{{newsTitle}}({{newsVotes}})</h1>
+        <p>{{newsBody}}</p>
+        <button @click="upVote"> Upvote </button><button @click="downVote"> Downvote </button><button @click="remove"> Remove </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "News",
+    props: {
+        newsTitle: {
+            type: String,
+            required: false,
+            default: ''
+        },
+        newsBody: {
+            type: String,
+            required: false,
+            default: ''
+        },
+        newsVotes: {
+            type: Number
+        }
+    },
+
+    methods: {
+        upVote(){
+            this.$emit('updateItem', this.newsTitle, this.newsVotes, 1)
+        },
+        downVote(){
+            this.$emit('updateItem', this.newsTitle, this.newsVotes, -1)
+        },
+        remove(){
+          this.$emit('removeItem', this.newsTitle)
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
