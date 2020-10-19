cb1e5d58097c9ec470202ab768db0f1ed96c6b2a
fokokouti <fokoboris@gmail.com>
2020-10-19 16:48:21 +0200

    Added NewsForm component, reviewed recommendation

--- exercises/1/webapp/src/components/Home.vue
+++ exercises/1/webapp/src/components/Home.vue
@@ -3,27 +3,18 @@
     <div class="news-list">
       <div class="news-list-wrapper">
         <h1>{{msg}}</h1>
-        <div class="news-holder" v-for="(newsContent, index) in sortedNewsList" :key="index">
-          <News :newsTitle="newsContent.title" :newsBody="newsContent.body" :newsVotes="newsContent.votes" @updateItem="mewsUpdateMessage" @removeItem="newsRemoveMessage"/>
+        <div class="news-holder" v-for="(news, index) in sortedNewsList" :key="index">
+          <News :news="news" @updateItem="newsUpdateMessage" @removeItem="newsRemoveMessage"/>
         </div>
       </div>
-      <form @submit.prevent="checkForm">
-        <p v-if="errors.length" class="alert">
-          <b>Please correct the following error(s):</b>
-          <ul>
-            <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
-          </ul>
-        </p>
-        <input v-model="newNews.title" placeholder="Title">
-        <input v-model="newNews.body" placeholder="Body">
-        <button type="submit"> Create </button>
-      </form>
+      <NewsForm :newsList="newsList" @createItem="newsCreated"/>
     </div>
   </div>
 </template>
 
 <script>
 import News from './News.vue'
+import NewsForm from './NewsForm.vue'
 
 export default {
   name: "Home",
@@ -74,75 +65,29 @@ export default {
     sortedNewsList: function() {
       // Sort by votes
       function compare(a, b) {
-        if (a.votes < b.votes)
-          return 1
-        if (a.votes > b.votes)
-          return -1
-        return 0
+        return b.votes - a.votes
       }
       const arrayClone = Array.from(this.newsList)
       return arrayClone.sort(compare)
     }
   },
   methods: {
-      mewsUpdateMessage(newsTitle, newsVotes, voteChange){
-        const newsId = this.newsList.findIndex((item) => item.title == newsTitle)
-        if (newsId != -1) {
-          console.log("New Update Message from Child (" + newsId + "), Votes: " + (newsVotes + voteChange))
-          this.newsList[newsId].votes = newsVotes + voteChange
-        }
-        else {
-          console.warn("News with title (" + newsTitle + ") not founded in the list of news")
-        }
+      newsUpdateMessage(newsTitle, newsVotes){
+        this.newsList = this.newsList.map((item) => {
+          if (item.title == newsTitle) item.votes = newsVotes
+          
+          return item
+        })
       },
       newsRemoveMessage(newsTitle){
-        const newsId = this.newsList.findIndex((item) => item.title == newsTitle)
-        if (newsId != -1) {
-          this.newsList.splice(newsId, 1)
-        }
-        else {
-          console.warn("News with title (" + newsTitle + ") not founded in the list of news")
-        }
+        this.newsList = this.newsList.filter((item) => item.title != newsTitle)
       },
-      add(){
-        this.newsList.push(
-          {
-            "title" : this.newNews.title,
-            "body" : this.newNews.body,
-            "votes" : 0
-          }
-        )
-        this.newNews.title = ""
-        this.newNews.body = ""
-      },
-      checkForm: function (e) {
-        if (this.newNews.title && this.newNews.title != '' && !this.newsInNewsList(this.newNews.title)) {
-          this.add()
-          this.errors = []
-          return true
-        }
-
-        this.errors = []
-
-        if (!this.newNews.title) {
-          this.errors.push('Title required.');
-        }
-
-        if (this.newsInNewsList(this.newNews.title)) {
-          this.errors.push('A news with title (' + this.newNews.title + ') is already contained in the list of news');
-        }
-
-        e.preventDefault();
-      },
-      newsInNewsList(newsTitle) {
-        const newsId = this.newsList.findIndex((item) => item.title == newsTitle)
-        if (newsId != -1)
-          return true
-        return false
+      newsCreated(news){
+        this.newsList.push({title: news.title, body: news.body, votes: 0})
       }
   },
   components: {
-    News
+    News, NewsForm
   }
 }
 </script>
@@ -159,11 +104,6 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
-}
-p.alert {
-  color: #a94442;
-  background-color: #f2dede;
-  border-color: #ebccd1;
 }
 a {
   color: #42b983;
---
+++ exercises/1/webapp/src/components/NewsForm.vue
@@ -0,0 +1,68 @@
+<template>
+    <form @submit.prevent="checkForm">
+        <p v-if="errors.length" class="alert">
+          <b>Please correct the following error(s):</b>
+          <ul>
+            <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+          </ul>
+        </p>
+        <input v-model="news.title" placeholder="Title">
+        <input v-model="news.body" placeholder="Body">
+        <button type="submit"> Create </button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "NewsForm",
+    props: {
+        newsList: {
+            type: Array,
+            required: true
+        }
+    },
+    data() {
+      return {
+            errors: [],
+            news: {
+                title : '',
+                body : ''
+            },
+        }
+    },
+    methods: {
+        checkForm: function (e) {
+            if (this.news.title && !this.newsInNewsList(this.news.title)) {
+                this.errors = []
+                this.$emit('createItem', this.news)
+                this.news = {title : '', body : ''}
+                return true
+            }
+
+            this.errors = []
+
+            if (!this.news.title) {
+                this.errors.push('Title required.');
+            }
+
+            if (this.newsInNewsList(this.news.title)) {
+                this.errors.push('A news with title (' + this.news.title + ') is already contained in the list of news');
+            }
+
+            e.preventDefault();
+        },
+        newsInNewsList(newsTitle) {
+            return this.newsList.map(n => n.title).includes(newsTitle)
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p.alert {
+  color: #a94442;
+  background-color: #f2dede;
+  border-color: #ebccd1;
+}
+</style>
