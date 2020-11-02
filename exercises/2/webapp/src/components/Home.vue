92a227a01ba9aa03db5069fe6a59eac9337ed849
Marcel <m.trattner99@gmail.com>
2020-11-02 18:29:06 +0100

    Setup of eslint, jest and Github Action
    Copy webapp folder into exercise 2 directory and install eslint with prettier and eslint-plugin-jest, jest and vue-test-utils
    and add a file to devine Github Actions. Jest is configured to also find tests in the src directory and eslint recognises them
    there as such. The configurations for eslint and jest are done in the package.json.

---
+++ exercises/2/webapp/src/components/Home.vue
@@ -0,0 +1,112 @@
+<template>
+  <div id="app">
+    <div class="news-list">
+      <div class="news-list-wrapper">
+        <h1>{{ msg }}</h1>
+        <div
+          class="news-holder"
+          v-for="(news, index) in sortedNewsList"
+          :key="index"
+        >
+          <News
+            :news="news"
+            @updateItem="newsUpdateMessage"
+            @removeItem="newsRemoveMessage"
+          />
+        </div>
+      </div>
+      <NewsForm :newsList="newsList" @createItem="newsCreated" />
+    </div>
+  </div>
+</template>
+
+<script>
+import News from "./News.vue";
+import NewsForm from "./NewsForm.vue";
+
+export default {
+  name: "Home",
+  props: {
+    msg: {
+      type: String,
+      required: false,
+      default: "Welcome to Hackernews"
+    },
+    newsListInput: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      errors: [],
+      newNews: {
+        title: "",
+        body: ""
+      },
+      newsList: []
+    };
+  },
+  computed: {
+    sortedNewsList: function() {
+      // Sort by votes
+      const compare = function(a, b) {
+        return b.votes - a.votes;
+      };
+      const arrayClone = Array.from(this.newsList);
+      return arrayClone.sort(compare);
+    }
+  },
+  methods: {
+    newsUpdateMessage(newsTitle, newsVotes) {
+      this.newsList = this.newsList.map(item => {
+        if (item.title == newsTitle)
+          return {
+            ...item,
+            votes: newsVotes
+          };
+
+        return item;
+      });
+    },
+    newsRemoveMessage(newsTitle) {
+      this.newsList = this.newsList.filter(item => item.title != newsTitle);
+    },
+    newsCreated(news) {
+      this.newsList.push({
+        title: news.title,
+        body: news.body,
+        votes: 0
+      });
+    }
+  },
+  beforeMount() {
+    this.newsList = this.newsListInput;
+  },
+  components: {
+    News,
+    NewsForm
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ exercises/2/webapp/src/components/NewsComponent.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from "@vue/test-utils";
+import News from "@/components/News.vue";
+
+describe("News.vue", () => {
+  it("renders title", () => {
+    const news = {
+      title: "TestTitle",
+      body: "",
+      votes: 0
+    };
+    const wrapper = shallowMount(News, {
+      propsData: { news }
+    });
+    expect(wrapper.text()).toMatch("TestTitle");
+  });
+});
