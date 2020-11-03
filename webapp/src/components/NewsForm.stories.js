347a6497ac80d7cabaae5282b6b55a21643552de
Marcel <m.trattner99@gmail.com>
2020-11-03 11:09:31 +0100

    Add Storybooks
    Created Storybooks for News and Newsforum component and refactored Github Action file.

---
+++ webapp/src/components/NewsForm.stories.js
@@ -0,0 +1,17 @@
+// News.stories.js
+
+import NewsForm from "./NewsForm";
+
+export default {
+  title: "Components/NewsForm",
+  component: NewsForm
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { NewsForm },
+  template: '<NewsForm :newsList="newsList" @submit.prevent="checkForm" />'
+});
+
+export const Default = Template.bind({});
+Default.args = {};
