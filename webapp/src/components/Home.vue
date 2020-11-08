f26b20ec840b0afeaf55b64c62006586d4bd57d2
Marcel <m.trattner99@gmail.com>
2020-11-08 20:56:43 +0100

    New feature in home
    button added in home component to change between ascending and descending
    sort order. Also a message is added which now appears when list is empty
    by utilizing a v-if. Test cases implemented for the home component to check behaviour
    for empty and not empty list and changing sort order.

--- webapp/src/components/Home.vue
+++ webapp/src/components/Home.vue
@@ -3,6 +3,8 @@
     <div class="news-list">
       <div class="news-list-wrapper">
         <h1>{{ msg }}</h1>
+        <button @click="switchSortOrder">reverse sort</button>
+        <h2 v-if="isEmpty()">The list is empty :(</h2>
         <div
           class="news-holder"
           v-for="(news, index) in sortedNewsList"
@@ -24,6 +26,11 @@
 import News from "./News.vue";
 import NewsForm from "./NewsForm.vue";
 
+const SortOrder = {
+  ASCENDING: 0,
+  DESCENDING: 1
+};
+
 export default {
   name: "Home",
   props: {
@@ -44,20 +51,38 @@ export default {
         title: "",
         body: ""
       },
-      newsList: []
+      newsList: [],
+      sortOrder: SortOrder.ASCENDING
     };
   },
   computed: {
     sortedNewsList: function() {
       // Sort by votes
-      const compare = function(a, b) {
+      const compareAscendent = function(a, b) {
         return b.votes - a.votes;
       };
+      const compareDescendent = function(a, b) {
+        return a.votes - b.votes;
+      };
       const arrayClone = Array.from(this.newsList);
-      return arrayClone.sort(compare);
+      return arrayClone.sort(
+        this.sortOrder == SortOrder.ASCENDING
+          ? compareAscendent
+          : compareDescendent
+      );
     }
   },
   methods: {
+    switchSortOrder() {
+      if (this.sortOrder == SortOrder.ASCENDING) {
+        this.sortOrder = SortOrder.DESCENDING;
+      } else {
+        this.sortOrder = SortOrder.ASCENDING;
+      }
+    },
+    isEmpty() {
+      return this.newsList.length <= 0;
+    },
     newsUpdateMessage(newsTitle, newsVotes) {
       this.newsList = this.newsList.map(item => {
         if (item.title == newsTitle)
