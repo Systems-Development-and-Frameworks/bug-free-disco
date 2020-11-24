26039b82ed4d7f14e8fd8cb82ab3b21f59413858
Marlon Kohlberger <marlonkohlberger2508@gmail.com>
2020-11-24 10:40:35 +0100

    [delete] backend

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-import Server from './server'
-
-const server = new Server()
-
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`)
-})
--- backend/posts.spec.js
+++
@@ -1,4 +0,0 @@
-import { createTestsClient } from 'apollo-server-testing'
-import server from './server'
-
-const { query, mutate } = createTestsClient(server)
